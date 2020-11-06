be5bb81c7fbdf7ac300658c1ed2a41525b443130
elikantor <eli.kantor@outlook.com>
2020-11-06 11:56:04 -0500

    added custom route for logo, styled logo component, and styled header component

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e9c51db85e00d665296c";
+/******/ 	var hotCurrentHash = "effcd84de18d10ce47eb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
