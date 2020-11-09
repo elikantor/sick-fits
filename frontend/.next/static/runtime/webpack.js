2c9444a11eecee47d73c62c122fc521f9334b527
elikantor <eli.kantor@outlook.com>
2020-11-09 12:35:03 -0500

    add global styling, route change visualization with NProgress, and Next's custom document to fix initial render styling

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "effcd84de18d10ce47eb";
+/******/ 	var hotCurrentHash = "38ff1c15adcb2866258a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
